4893f7dbcd56f4d23f17da5bc1a23e979ff16264
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-14 21:22:34 +0300

    configured the urlpatterns for the media files in the urls.py in the project folder. modified index.html by adding a bootstrap card

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,6 @@ from .models import Project
 # Create your views here.
 def home(request):
     posts = Project.objects.all()
-
     context = {'posts': posts}
     return render(request, 'projects/index.html', context)
     
